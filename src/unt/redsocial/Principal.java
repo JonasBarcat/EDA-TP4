0b6a9203dba05800f40f464ed802b8ce7641ee31
Jony <jonas_barcat.06@hotmail.com>
2019-04-14 19:01:31 -0300

    2do commit RedSocial: estaUsuario, echarUsuario

--- src/unt/redsocial/Principal.java
+++ src/unt/redsocial/Principal.java
@@ -13,10 +13,25 @@ public class Principal {
     
     public static void main(String[] args){
     
+        Usuario usr1=new Usuario(1);
+        Usuario usr2=new Usuario(2);
+        Usuario usr3=new Usuario(3);
+        Usuario usr4=new Usuario(4);
+        
+        
         RedSocial Facebook=new RedSocial();
-        Facebook.registrar(1);
-        Facebook.registrar(2);
+        Facebook.registrar(usr1);
+        Facebook.registrar(usr2);
+        Facebook.registrar(usr3);
+        Facebook.registrar(usr4);
         Facebook.verUsuarios();
+        
+        if(Facebook.estaUsuario(usr3)){
+         System.out.println("El usuario "+usr3.getUsuario()+" si se encuantra en el sistema");
+        }else System.out.println("El usuario no se encuentra en el sistema");
+        
+        Facebook.echarUsuario(usr2);
+        
     }
     
 }
--- src/unt/redsocial/RedSocial.java
+++ src/unt/redsocial/RedSocial.java
@@ -18,26 +18,53 @@ public class RedSocial {
     
     
     
-    public void registrar(int numerodeusuario){ 
-        Usuario nuevo=new Usuario(numerodeusuario);
-        nuevo.setSiguiente(this.primero);
-        this.primero=nuevo;
+    public void registrar(Usuario usr){  // cargo un nuevo usuario(usr) a la red social
+        usr.setSiguiente(this.primero);
+        this.primero=usr;
     }
     
     
-    public boolean esVacia(){
+    public boolean esVacia(){  // retorna verdadero o falso si la lista(red social) está vacía o no
         if(this.primero==null){
             return true;
         }else return false;
     }
     
     
-    public void verUsuarios(){
+    public void verUsuarios(){   // veo los usuarios cargados en la red social
         Usuario aux=this.primero;
         System.out.print("Usuarios cargados en esta red social: ");
         while(aux!=null){
             System.out.print("->"+aux.getUsuario());
             aux=aux.getSiguiente();
+        }
+        System.out.println();
+    }
+    
+    public boolean estaUsuario(Usuario usr){ // ver si el usuario enviado por parametro está en la red social
+        Usuario aux=this.primero;    
+            while(aux!=null){
+                if(usr.equals(aux)){
+                    return true;}
+            aux=aux.getSiguiente();
+            }
+        return false;
+    }
+    
+    
+    
+    public void echarUsuario(Usuario usr){  //elimina el usuario enviado por parametro de la red social
+        if(esVacia()){
+            System.out.println("No se pudo echar al usuario, la Red Social no tiene usuarios");
+        }else{
+            Usuario aux=this.primero;
+                while(aux!=null){
+                    if(aux.getSiguiente().equals(usr)){
+                        aux.setSiguiente(aux.getSiguiente().getSiguiente());
+                        System.out.println("El usuario : "+usr.getUsuario()+" fue echado exitosamente");
+                    }
+                aux=aux.getSiguiente();
+                }
         }
     }
     
